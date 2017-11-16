ec515c996aa3adb7780c99bdd3bd0af3d716fec0
Aleksey Zgurskiy <mr.noone@icloud.com>
2017-11-16 09:08:43 +0000

    Initial commit

---
+++ EasyDIKit/EasyDIKit/EasyDIKit.h
@@ -0,0 +1,12 @@
+//
+//  EasyDIKit.h
+//  EasyDIKit
+//
+//  Created by Aleksey Zgurskiy on 16.11.2017.
+//  Copyright © 2017 Aleksey Zgurskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double EasyDIKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char EasyDIKitVersionString[];
